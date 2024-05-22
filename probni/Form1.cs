3a744bce46dda2d3e7df034567f54812d3b86431
yanzhi021 <yanzhi.jin@skole.hr>
2024-05-22 17:07:50 +0200

    Dodan novi botun

--- probni/Form1.cs
+++ probni/Form1.cs
@@ -6,5 +6,10 @@ namespace probni
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dobro");
+        }
     }
 }
